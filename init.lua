19cd62d4eeda4eb122bc68bb300e606aac13fac9
meme <uesd.fun@gmail.com>
2025-06-19 01:22:58 +0800

    sth

--- init.lua
+++ init.lua
@@ -98,11 +98,13 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-------------------------------------------------------------------------------
 vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
-vim.o.list = false
+vim.o.list = false -- show <Tab> and <EOL>
+-------------------------------------------------------------------------------
 
 -- Make line numbers default
 vim.o.number = true
