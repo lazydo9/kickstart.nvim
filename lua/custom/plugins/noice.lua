aea094499c216ea5d27c94f010bb09f43332a3c9
meme <uesd.fun@gmail.com>
2025-06-18 16:51:17 +0800

    rename

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -6,9 +6,9 @@ return {
   {
     'folke/noice.nvim',
     event = 'VeryLazy',
-    opts = {
-      -- add any options here
-    },
+    -- opts = {
+    --   -- add any options here
+    -- },
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       'MunifTanjim/nui.nvim',
