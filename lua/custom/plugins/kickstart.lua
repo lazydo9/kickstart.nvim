ef574e7e39b051f4a57e160a68d5ae16977d2b91
meme <uesd.fun@gmail.com>
2025-06-22 16:53:44 +0800

    kick

--- lua/custom/plugins/kickstart.lua
+++ lua/custom/plugins/kickstart.lua
@@ -1,8 +1,2 @@
 return {
-   -- require 'kickstart.plugins.debug',
-   -- require 'kickstart.plugins.indent_line',
-   -- require 'kickstart.plugins.lint',
-   -- require 'kickstart.plugins.autopairs',
-   require 'kickstart.plugins.neo-tree',
-   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 }
